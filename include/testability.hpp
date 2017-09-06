921ec4d25e8e0d07e74941aa7d84c140b22ff890
bovoignazio <bovo.ignazio.dev@gmail.com>
2017-09-06 23:05:30 +0200

    added comments

--- include/testability.hpp
+++ include/testability.hpp
@@ -69,17 +69,17 @@ decltype(auto) th::min_p_(const unsigned n1, const unsigned n2, const Frequency 
 
   auto N = n1 + n2;
   BOOST_ASSERT(f < N); // precondition
-  auto op2 = [](const auto a, const auto b) {
+  auto op2 = [](const auto a, const auto b) { // correctly compute n/m
     return static_cast<double>(a) / static_cast<double>(b);};
 
-  auto init_v = static_cast<double>(n1) / static_cast<double>(N);
+  auto init_v = static_cast<double>(n1) / static_cast<double>(N); // init value
 
   auto base1 = f > n1 ? 1u : n1 - f + 1;
   auto base2 = f > n1 ? N - n1 + 1 : N - f + 1;
 
-  auto r1 = view::iota(base1,n1);
-  auto r2 = view::iota(base2, N);
-  return inner_product(r1, r2, init_v, std::multiplies<double>{}, op2);
+  auto r1 = view::iota(base1,n1); // {base1, base1+1, ..., n1}
+  auto r2 = view::iota(base2, N); // {base2, base2+1, ..., N}
+  return inner_product(r1, r2, init_v, std::multiplies<double>{}, op2); // calculate base1/base2 x (base1 + 1)/(base2 + 1) x ... x n1/N
 }
 
 
@@ -89,21 +89,22 @@ th::one_pass_(Algorithm alg, const unsigned n1, const unsigned n2, const double 
 {
   using namespace ranges;
 
-  auto phi = [=](const auto f){return th::min_p_(n1, n2, f);};
+  auto phi = [=](const auto f){return th::min_p_(n1, n2, f);}; // minimum p-value function
   auto freq_rng = view::iota(1u,n1) | view::remove_if([=](const auto f){
-      return phi(f) > alpha;});
-  auto min_freq = *begin(freq_rng);
+      return phi(f) > alpha;}); 
+  auto min_freq = *begin(freq_rng); // min. admissible frequency for mining at significance level: alpha
 
-  auto freqs = alg(min_freq); BOOST_ASSERT(size(freqs) > 0);
+  auto freqs = alg(min_freq); BOOST_ASSERT(size(freqs) > 0); //run the mining algorithm
+  sort(freqs); // then sort (increasing order)
 
   auto map_ = view::zip(view::iota(1ul, size(freqs)), freqs)
     | view::adjacent_remove_if([](const auto& a, const auto& b){
-	return std::get<1>(a) == std::get<1>(b);});
+	return std::get<1>(a) == std::get<1>(b);}); // enumerate the mapping (freq, m(freq))
 
   auto pred = [=](const auto& p){
-    return phi(std::get<1>(p)) * std::get<0>(p) <= alpha;};
+    return phi(std::get<1>(p)) * std::get<0>(p) <= alpha;}; // search for the SIGMA root frequency
 
-  return std::get<1>(*find_if(map_, pred));
+  return std::get<1>(*find_if(map_, pred)); // and return it
 }
 
 template<typename Algorithm>
@@ -111,13 +112,14 @@ decltype(auto)
 th::lamp_dec_(Algorithm m, const unsigned n1, const unsigned n2, const double alpha) 
 {
   using namespace ranges;
-  auto freq_rng = view::iota(1u,n1) | view::reverse;
-  auto phi = [=](const auto f){return th::min_p_(n1, n2, f);};
+  auto freq_rng = view::iota(1u,n1) | view::reverse; // {n1, n1 - 1, ..., 1}
+  auto phi = [=](const auto f){return th::min_p_(n1, n2, f);}; // minimum p-value function
   auto pred = [=](const auto f){
     const auto m_ = m(f); BOOST_ASSERT(m > 0); // <-- very important!
-    return m_ * phi(f) > alpha;};
+    return m_ * phi(f) > alpha;}; 
 
-  return *find_if(freq_rng, pred) - 1;
+  // find the first value f for which m(f) * phi(f) <= alpha is satisfied and return it  
+  return *find_if(freq_rng, pred) - 1; 
 }
 
 template<typename Algorithm>
@@ -129,11 +131,13 @@ th::early_term_(Algorithm m_et, const unsigned n1, const unsigned n2, const doub
     const auto pv = phi(f);
     auto m =  m_et(f, pv); BOOST_ASSERT(m > 0); // <-- very important!
     return m * pv <= alpha;
-  };
+  }; // continue the meaning process until m * pv <= alpha is satisfied
 
+  // min. admissible range of frequency  
   auto freq_rng = view::iota(1,n1)
-    | view::remove_if([=](const auto f){return phi(f) > alpha;});
+    | view::remove_if([=](const auto f){return phi(f) > alpha;}); 
 
+  // return the first freq that satisfies the predicate pred  
   return *find_if(freq_rng, pred);
 }
 
@@ -145,22 +149,25 @@ th::bis_leap_(Algorithm m_et, const unsigned n1, const unsigned n2, const double
   using namespace boost::math;
     
   auto phi = [=](const auto f){return th::min_p_(n1, n2, f);};
-  auto g = [=](const auto f){
+  auto g = [=](const auto f){ // function whose root is to be found
     const auto pv = phi(f);
     const auto m = m_et(f, pv); BOOST_ASSERT(m > 0); // <-- very important!
     return m * pv - alpha;};
 
   auto freq_rng = view::iota(1u,n1)| view::remove_if([=](const auto f){
-      return phi(f) > alpha;});
-    
+      return phi(f) > alpha;}); // min. admissible frequency range
+
+  // setup [freq_min, freq_max] interval
   auto min = static_cast<double>(*begin(freq_rng));
   auto max = static_cast<double>(n1);
-    
+
+  // termination condition freq_max - freq_min == 1
   auto tol = [](const auto a, const auto b){
     return b - a <= 1;};
 
+  // apply the bisection method and return freq_min 
   auto res =  tools::bisect(g, min, max, tol);
-  return std::floor(res.first);
+  return std::floor(res.first); 
 }
 // }   
 
